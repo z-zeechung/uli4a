553022b26c2bb8ebaa03d3b1d91e3de8d6a93f8f
z-zeechung <zhouzeechung@qq.com>
2024-05-31 17:25:29 +0800

    -

--- src/string/memccpy.c
+++ src/string/memccpy.c
@@ -1,11 +1,11 @@
 # include <string.h>
 
-void* uli4a_memccpy(void *restrict dest, const void *restrict src, int c, size_t n) {
+void* uli4a_memccpy(void* dest, const void* src, int c, size_t n) {
     unsigned char *d = dest;
     const unsigned char *s = src;
     
     // 循环，直到复制了 n 个字节，或者找到了字符 c
-    for (size_t i = 0; i < n; i++) {
+    for(size_t i = 0; i < n; i++) {
         d[i] = s[i];  // 复制字节
         if (s[i] == (unsigned char)c) {
             // 如果找到了字符 c，返回目标字符串中该字符的位置
