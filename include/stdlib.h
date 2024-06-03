70ec74bb571a136ec373468fb70f7c8d827c2f33
z-zeechung <152474942+z-zeechung@users.noreply.github.com>
2024-06-03 19:58:26 +0800

    Update stdlib.h

---
+++ include/stdlib.h
@@ -0,0 +1,12 @@
+// TODO
+
+# ifndef H_ULI4A_STDLIB
+# define H_ULI4A_STDLIB
+
+        # include <uli4a/mem.h>
+
+        // malloc defined in <uli4a/mem.h>
+
+        // free defined in <uli4a/mem.h>
+
+# endif
