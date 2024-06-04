02fa5d357fc76249beceb334486a42c0eaacac49
z-zeechung <152474942+z-zeechung@users.noreply.github.com>
2024-06-04 14:56:44 +0800

    Update limits.h

--- include/limits.h
+++ include/limits.h
@@ -3,10 +3,4 @@
 # ifndef H_ULI4A_LIMITS
 # define H_ULI4A_LIMITS
 
-	# define INT_MAX ((int)(~(unsigned int)0 >> 1))
-
-	# define UINT_MAX ((unsigned int)-1)
-	
-	# define ULONG_MAX ((unsigned long)-1L)
-
 # endif
